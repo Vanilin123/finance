d015e323caed00e5064d39f482522009cedaa005
Vanilin123 <149298214+Vanilin123@users.noreply.github.com>
2024-05-15 12:59:28 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
+ output: 'export',
+    // Define the getStaticPaths function for dynamic routes
+    async exportPathMap() {
+        // Add any paths you need to generate here
+        const paths = {
+            '/': { page: '/' }, // Example root path
+            // Add other routes as needed
+        };
+        return paths;
+    },
 };
 
 export default nextConfig;
